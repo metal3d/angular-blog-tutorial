ed10c206788eea151d04b4fc050bdae4d0c4ea30
Patrice FERLET <metal3d@gmail.com>
2015-12-09 22:06:11 +0100

    List posts

--- www/blog/app/scripts/controllers/main.js
+++ www/blog/app/scripts/controllers/main.js
@@ -11,7 +11,4 @@ angular.module('blogApp')
   .controller('MainCtrl', function ($scope, Blog) {
       var posts = Blog.getPosts();
       $scope.posts = posts;
-
-      // en admettant que le post "1" existe:
-      $scope.firstPost = Blog.getPost(1);
   });
